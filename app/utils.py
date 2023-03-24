6f175bd0207ec42246119d2ab4d722e32227903e
mutantsan <kvaqich@gmail.com>
2023-03-24 22:17:40 +0200

    create utils to create/fetch user

---
+++ app/utils.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any
+
+from app.model import Session, User
+
+
+def create_user(user_data: dict[str, Any]) -> User:
+    user: User = User(**user_data)
+    Session.add(user)
+    Session.commit()
+
+    return user
+
+
+def get_user(user_id: int) -> User | None:
+    return Session.get(User, user_id)
